fbd4dde6450b8b367bf0eb3aef2b4a9f5fec5ad9
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2021-08-09 15:30:32 +0700

    Code cleaning

--- src/components/jumbotron.js
+++ src/components/jumbotron.js
@@ -5,7 +5,6 @@ import line from "../assets/img/line.svg";
 import linkedin from "../assets/img/linkedin.svg";
 import instagram from "../assets/img/instagram.svg";
 import telegram from "../assets/img/telegram.svg";
-import download from "../assets/img/download.svg";
 import { FiMail } from "react-icons/fi";
 import { AiOutlineCloudDownload } from "react-icons/ai";
 
